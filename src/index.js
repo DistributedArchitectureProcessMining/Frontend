d77b97a62bf8bd90ebd91b4c644794201e8415c0
Andrea Burattin <delas@users.noreply.github.com>
2023-11-28 14:56:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { render } from "react-dom";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -15,12 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // This is the root component of our app. The router is used to create different pages. 
   // Currently all of PDL is located at "Home" and effectively works as a single page app.
-  <BrowserRouter>
+  <Router>
         <Routes>
             <Route path="Page1" element={<App page = {"Page1"} />} />
             <Route path="/" element={<App page = {"Home"}/>}/>
         </Routes>
-    </BrowserRouter>
+    </Router>
 );
 
 reportWebVitals();
